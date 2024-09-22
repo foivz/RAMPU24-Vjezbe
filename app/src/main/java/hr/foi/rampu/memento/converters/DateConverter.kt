89ddcff4babb36cf302ae816e00c98d58280de73
ktakac <ktakac@foi.hr>
2024-09-22 17:09:43 +0200

    logic for showing and saving tasks to Room database

---
+++ app/src/main/java/hr/foi/rampu/memento/converters/DateConverter.kt
@@ -0,0 +1,17 @@
+package hr.foi.rampu.memento.converters
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class DateConverter {
+
+    @TypeConverter
+    fun fromDate(date: Date): Long {
+        return date.time
+    }
+
+    @TypeConverter
+    fun toDate(timeStamp: Long): Date {
+        return Date(timeStamp)
+    }
+}
--- app/src/main/java/hr/foi/rampu/memento/fragments/CompletedFragment.kt
+++ app/src/main/java/hr/foi/rampu/memento/fragments/CompletedFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hr.foi.rampu.memento.R
+import hr.foi.rampu.memento.adapters.TasksAdapter
+import hr.foi.rampu.memento.database.TasksDatabase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,6 +23,8 @@ private const val ARG_PARAM2 = "param2"
  */
 class CompletedFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    private lateinit var recyclerView: RecyclerView
+    private val tasksDAO = TasksDatabase.getInstance().getTasksDao()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,5 +37,18 @@ class CompletedFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_completed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = view.findViewById(R.id.rv_completed_tasks)
+        val completedTasks = tasksDAO.getAllTasks(true)
+        recyclerView.adapter = TasksAdapter(completedTasks.toMutableList())
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+        parentFragmentManager.setFragmentResultListener("task_completed", viewLifecycleOwner) { _, bundle ->
+            val addedTaskId = bundle.getInt("task_id")
+            val tasksAdapter = recyclerView.adapter as TasksAdapter
+            tasksAdapter.addTask(tasksDAO.getTask(addedTaskId))
+        }
     }
 }
