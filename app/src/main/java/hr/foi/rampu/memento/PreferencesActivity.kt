0709135ac269003f08bc567d1b918d56cab77dbc
ktakac <ktakac@foi.hr>
2024-12-11 00:45:04 +0100

    topic logic added

--- app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
+++ app/src/main/java/hr/foi/rampu/memento/PreferencesActivity.kt
@@ -2,17 +2,20 @@ package hr.foi.rampu.memento
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.messaging.FirebaseMessaging
 import hr.foi.rampu.memento.fragments.MementoSettingsFragment
 
 const val RESULT_LANG_CHANGED = AppCompatActivity.RESULT_FIRST_USER
 class PreferencesActivity : AppCompatActivity(),
         SharedPreferences.OnSharedPreferenceChangeListener
 {
+    private val possibleSubscriptions = listOf("RAMPU", "RWA", "RPP")
 
     companion object {
         fun switchDarkMode(isDarkModeSelected: Boolean?) {
@@ -52,12 +55,27 @@ class PreferencesActivity : AppCompatActivity(),
         when (key) {
             "preference_dark_mode" -> switchDarkMode(sharedPreferences?.getBoolean(key, false))
             "preference_language" -> notifyLanguageChangedAndClose()
+            "preference_subscribed_categories" -> manageFCMSubscriptions(
+                sharedPreferences?.getStringSet(key, setOf())!!
+            )
         }
     }
 
     private fun notifyLanguageChangedAndClose() {
         setResult(RESULT_LANG_CHANGED)
         finish()
+    }
+
+    private fun manageFCMSubscriptions(selectedTopics: Set<String>) {
+        possibleSubscriptions.forEach { availableTopic ->
+            if (selectedTopics.contains(availableTopic)) {
+                Log.i("MEMENTO_TOPICS", "Subscribing to $availableTopic")
+                FirebaseMessaging.getInstance().subscribeToTopic(availableTopic)
+            } else {
+                Log.i("MEMENTO_TOPICS", "Unsubscribing from $availableTopic")
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(availableTopic)
+            }
+        }
     }
 
 
