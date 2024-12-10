ac1f05f3c6d928e83a98840fac3e46d88f632221
ktakac <ktakac@foi.hr>
2024-12-11 00:31:41 +0100

    http method adde

--- app/src/main/java/hr/foi/rampu/memento/ws/NewsService.kt
+++ app/src/main/java/hr/foi/rampu/memento/ws/NewsService.kt
@@ -2,9 +2,13 @@ package hr.foi.rampu.memento.ws
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsService {
 
     @GET("news.php")
     fun getNews(): Call<NewsResponse>
+
+    @GET("news.php")
+    fun getNews(@Query("title") newsTitle: String): Call<NewsItem>
 }
